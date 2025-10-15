a71dd540c6d7957233a042b1ac9043f62fef37f6
Murillo Oliveira <murillo1047@gmail.com>
2025-10-15 13:24:46 -0300

    ajustes

--- database/db.go
+++ database/db.go
@@ -15,15 +15,15 @@ var (
 
 func ConectaComBancoDeDados() {
 	endereco := os.Getenv("DB_HOST")
-    usuario := os.Getenv("DB_USER")
-    senha := os.Getenv("DB_PASSWORD")
-    nomeBanco := os.Getenv("DB_NAME")
-    portaBanco := os.Getenv("DB_PORT")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
 	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
-    DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+	_ = DB.AutoMigrate(&models.Aluno{})
 }
